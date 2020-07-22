ca7e59a9260285f92339f3f92905509efd2f2071
rabah babaci <4rabah@gmail.com>
2020-07-22 16:17:06 -0700

    create the remove_head() for singly linked list

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,19 +1,19 @@
 class Node:
-    def __init__(self, value=None, next=None):
+    def __init__(self, value=None, next_node=None):
         self.value = value
-        self.next = next
+        self.next_node = next_node
 
     def get_value(self):
         return self.value
 
     def get_next(self):
-        return self.next
+        return self.next_node
 
     def set_value(self, newValue):
         self.value = newValue
 
     def set_next(self, newNext):
-        self.next = newNext
+        self.next_node = newNext
 
 
 class LinkedList:
@@ -23,9 +23,33 @@ class LinkedList:
 
     def add_to_tail(self, value):
         new_node = Node(value)
-        if self.tail == None:
+        if not self.tail:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.set_next(new_node)
             self.tail = new_node
+
+    def contains(self, value):
+        if not self.head:
+            return False
+        else:
+            current_node = self.head
+            while current_node:
+                if current_node.get_value() == value:
+                    return True
+                current_node = current_node.get_next()
+            return False
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        elif not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
