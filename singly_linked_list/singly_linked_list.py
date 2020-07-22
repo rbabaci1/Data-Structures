3f8bae2461b6c3ddaacb208cfa0c936f19fa9329
rabah babaci <4rabah@gmail.com>
2020-07-22 15:42:00 -0700

    define the add_to_tail method in LinkedList

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -3,14 +3,29 @@ class Node:
         self.value = value
         self.next = next
 
-    def getValue(self):
+    def get_value(self):
         return self.value
 
-    def getNext(self):
+    def get_next(self):
         return self.next
 
-    def setValue(self, newValue):
+    def set_value(self, newValue):
         self.value = newValue
 
-    def setNext(self, newNext):
+    def set_next(self, newNext):
         self.next = newNext
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.tail == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
