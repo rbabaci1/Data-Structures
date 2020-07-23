a44366d1cdd70ad15a5d1b615594f3b53fce914b
rabah babaci <4rabah@gmail.com>
2020-07-22 19:32:47 -0700

    create the Queue using an array

--- stack/stack.py
+++ stack/stack.py
@@ -35,19 +35,19 @@ class Stack:
 # Implemented using a list as the storage
 
 
-# class Stack:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
+class ArrayStack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
 
-#     def __len__(self):
-#         return self.size
+    def __len__(self):
+        return self.size
 
-#     def push(self, value):
-#         self.storage.append(value)
-#         self.size += 1
+    def push(self, value):
+        self.storage.append(value)
+        self.size += 1
 
-#     def pop(self):
-#         if self.size:
-#             self.size -= 1
-#             return self.storage.pop()
+    def pop(self):
+        if self.size:
+            self.size -= 1
+            return self.storage.pop()
