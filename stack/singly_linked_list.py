40f68a47fa444670d03d1f2ec4cb67e08f68b0c2
rabah babaci <4rabah@gmail.com>
2020-07-22 18:44:21 -0700

    finish the Stack linked list implementation

---
+++ stack/singly_linked_list.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_value(self, newValue):
+        self.value = newValue
+
+    def set_next(self, newNext):
+        self.next_node = newNext
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_head(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
+    def remove_head(self):
+        if not self.head:
+            return None
+        elif not self.head.get_next():
+            head = self.head
+            self.head = None
+            self.tail = None
+            return head.get_value()
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            return value
--- stack/stack.py
+++ stack/stack.py
@@ -1,5 +1,4 @@
 from singly_linked_list import LinkedList
-
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order.
@@ -25,14 +24,17 @@ class Stack:
         return self.size
 
     def push(self, value):
-        self.storage.add_to_tail(value)
+        self.storage.add_to_head(value)
         self.size += 1
 
     def pop(self):
-        self.storage.r
-
+        if self.size:
+            self.size -= 1
+            return self.storage.remove_head()
 
 # Implemented using a list as the storage
+
+
 # class Stack:
 #     def __init__(self):
 #         self.size = 0
