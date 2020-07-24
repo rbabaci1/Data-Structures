c0a805703d803346a3491e8dbb15a92be908357f
rabah babaci <4rabah@gmail.com>
2020-07-24 12:21:19 -0700

    Refactor and DRY singly linked list

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -26,42 +26,38 @@ class LinkedList:
 
     def add_to_tail(self, value):
         new_node = Node(value)
-        if not self.tail:
-            self.set_head_tail(new_node, new_node)
-        else:
+        if self.tail:
             self.tail.set_next(new_node)
             self.tail = new_node
+        else:
+            self.set_head_tail(new_node, new_node)
 
     def add_to_head(self, value):
         new_node = Node(value)
-        if not self.head:
-            self.set_head_tail(new_node, new_node)
-        else:
+        if self.head:
             new_node.set_next(self.head)
             self.head = new_node
+        else:
+            self.set_head_tail(new_node, new_node)
 
     def contains(self, value):
-        if not self.head:
-            return False
-        else:
+        if self.head:
             current_node = self.head
             while current_node:
                 if current_node.get_value() == value:
                     return True
                 current_node = current_node.get_next()
-            return False
+        return False
 
     def remove_head(self):
-        if not self.head:
-            return None
-        if not self.head.get_next():
-            head_value = self.head.get_value()
-            self.set_head_tail(None, None)
-            return head_value
-        else:
+        if self.head and self.head.get_next():
             value = self.head.get_value()
             self.head = self.head.get_next()
             return value
+        elif self.head and not self.head.get_next():
+            head_value = self.head.get_value()
+            self.set_head_tail(None, None)
+            return head_value
 
     def get_max(self):
         if self.head:
