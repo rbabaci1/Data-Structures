eca2ec08a90e5224da378277f9c430503e8d3d9d
rabah babaci <4rabah@gmail.com>
2020-07-23 19:55:26 -0700

    finish get_max

--- queue/queue.py
+++ queue/queue.py
@@ -26,19 +26,16 @@ Stretch: What if you could only use instances of your Stack class to implement t
 
 class ArrayQueue:
     def __init__(self):
-        self.size = 0
         self.storage = []
 
     def __len__(self):
-        return self.size
+        return len(self.storage)
 
     def enqueue(self, value):
         self.storage.append(value)
-        self.size += 1
 
     def dequeue(self):
         if self.size:
-            self.size -= 1
             return self.storage.pop(0)
 
 
