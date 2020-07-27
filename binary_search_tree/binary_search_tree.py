43f1e0d473ac70df24115294f9cad4e379f09faf
rabah babaci <4rabah@gmail.com>
2020-07-27 14:39:16 -0700

    finish implementing the rest of the methods

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,8 +1,3 @@
-# import sys
-
-# sys.path.append("../queue")
-# from queue import Queue
-
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -10,28 +5,27 @@ at searching for a particular piece of data in the tree.
 
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
-   on the BSTNode class.
+   on the BinarySearchTree class.
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
-   on the BSTNode class.
+   on the BinarySearchTree class.
 """
 
 
-class BSTNode:
+class BinarySearchTree:
     def __init__(self, value):
         self.value = value
         self.left = self.right = None
-        # self.right = None
 
     # Insert the given value into the tree
     def insert(self, value):
         if value < self.value:
             if not self.left:
-                self.left = BSTNode(value)
+                self.left = BinarySearchTree(value)
             else:
                 self.left.insert(value)
         else:
             if not self.right:
-                self.right = BSTNode(value)
+                self.right = BinarySearchTree(value)
             else:
                 self.right.insert(value)
 
@@ -63,67 +57,93 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        # implemented using a recursive Preorder depth first traversal
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+
+    # Part 2 -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    # implemented using InOrder depth first traversal
+    def in_order_dft(self):
+        if self.left:
+            self.left.in_order_dft()
+        print(self.value)
+        if self.right:
+            self.right.in_order_dft()
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self):
         queue = []
         queue.append(self)
         while len(queue):
             current = queue[0]
-            fn(current.value)
+            print(current.value)
             if current.left:
                 queue.append(current.left)
             if current.right:
                 queue.append(current.right)
             queue.pop(0)
 
-    # Part 2 -----------------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        pass
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self):
-        pass
-
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self):
-        pass
+        stack = []
+        stack.append(self)
+        while len(stack):
+            root = stack.pop()
+            print(root.value)
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                stack.append(root.left)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self):
-        pass
+        print(self.value)
+        if self.left:
+            self.left.pre_order_dft()
+        if self.right:
+            self.right.pre_order_dft()
 
     # Print Post-order recursive DFT
     def post_order_dft(self):
-        pass
+        if self.left:
+            self.left.post_order_dft()
+        if self.right:
+            self.right.post_order_dft()
+        print(self.value)
 
 
 """
 This code is necessary for testing the `print` methods
 """
-# bst = BinarySearchTree(1)
-# bst = BSTNode(1)
+bst = BinarySearchTree(1)
 
-# bst.insert(8)
-# bst.insert(5)
-# bst.insert(7)
-# bst.insert(6)
-# bst.insert(3)
-# bst.insert(4)
-# bst.insert(2)
+bst.insert(8)
+bst.insert(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(3)
+bst.insert(4)
+bst.insert(2)
 
-# bst.bft_print()
-# bst.dft_print()
 
-# print("elegant methods")
-# print("pre order")
-# bst.pre_order_dft()
-# print("in order")
-# bst.in_order_dft()
-# print("post order")
-# bst.post_order_dft()
+bst.bft_print()
+bst.dft_print()
+
+print("elegant methods")
+print("pre order")
+bst.pre_order_dft()
+print("in order")
+bst.in_order_dft()
+print("post order")
+bst.post_order_dft()
